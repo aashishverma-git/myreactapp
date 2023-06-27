36cc7346ad8134bcdb3c61b573ac0f51535a6238
aashishverma-git <aashish23verma@gmail.com>
2023-06-27 09:28:54 +0530

    modified app js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Aashish Verma
+         Deploy react app using github actions.... 
         </a>
       </header>
     </div>
